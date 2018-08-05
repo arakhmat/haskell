51a08f5d2981849a55fa21529acb6e773f71225e
Akhmed Rakhmati <akhmed.rakhmati@gmail.com>
2018-08-05 19:50:10 -0400

    - Finished BST.

--- exercises/binary-search-tree/src/BST.hs
+++ exercises/binary-search-tree/src/BST.hs
@@ -10,28 +10,40 @@ module BST
     , toList
     ) where
 
-data BST a = Dummy deriving (Eq, Show)
+data BST a = Leaf | Node (BST a) a (BST a) deriving (Eq, Show)
 
 bstLeft :: BST a -> Maybe (BST a)
-bstLeft tree = error "You need to implement this function."
+bstLeft Leaf = Nothing
+bstLeft (Node left _ _) = Just left
 
 bstRight :: BST a -> Maybe (BST a)
-bstRight tree = error "You need to implement this function."
+bstRight Leaf = Nothing
+bstRight (Node _ _ right) = Just right
 
 bstValue :: BST a -> Maybe a
-bstValue tree = error "You need to implement this function."
+bstValue Leaf = Nothing
+bstValue (Node _ value _) = Just value
 
 empty :: BST a
-empty = error "You need to implement this function."
+empty = Leaf
+
+fromReversedList :: Ord a => [a] -> BST a
+fromReversedList [] = empty
+fromReversedList [x] = singleton x
+fromReversedList (x:xs) = insert x (fromReversedList xs)
 
 fromList :: Ord a => [a] -> BST a
-fromList xs = error "You need to implement this function."
+fromList xs = fromReversedList (reverse xs)
 
 insert :: Ord a => a -> BST a -> BST a
-insert x tree = error "You need to implement this function."
+insert x Leaf = singleton x
+insert x (Node left value right)
+    | x <= value = Node (insert x left) value right
+    | otherwise = Node left value (insert x right)
 
 singleton :: a -> BST a
-singleton x = error "You need to implement this function."
+singleton x = Node Leaf x Leaf
 
 toList :: BST a -> [a]
-toList tree = error "You need to implement this function."
+toList Leaf = []
+toList (Node left value right) = toList left ++ [value] ++ toList right
